2a412d2d559f93e42b3145d40c96c09b62815c4c
sgoenko <sgoenko@gmail.com>
2021-05-09 13:38:01 +0300

    Filter on tree entities added.

--- src/main/java/com/example/TestFilter/EmployeeDto.java
+++ src/main/java/com/example/TestFilter/EmployeeDto.java
@@ -2,28 +2,26 @@ package com.example.TestFilter;
 
 public class EmployeeDto {
 
-	private String employeeName;
-	private int salary;
-	private int age;
-	private String departmant;
-	private String vocation;
+    private String employeeName;
+    private int salary;
+    private int age;
+    private String departmant;
+    private String vocation;
 
-	public EmployeeDto(Employee emp, Department dep, Vocation vocation) {
-		this.employeeName = emp.getName();
-		this.salary = emp.getSalary();
-		this.age = emp.getAge();
-		this.departmant = dep.getName();
-		this.vocation = vocation.getName();
-	}
+    public EmployeeDto(Employee emp, Department dep, Vocation vocation) {
+        this.employeeName = emp.getName();
+        this.salary = emp.getSalary();
+        this.age = emp.getAge();
+        this.departmant = dep.getName();
+        this.vocation = vocation.getName();
+    }
 
-	@Override
-	public String toString() {
-		return "EmployeeDto{" +
-				"employeeName='" + employeeName + '\'' +
-				", salary=" + salary +
-				", age=" + age +
-				", departmant=" + departmant +
-				", vocation=" + vocation +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return employeeName + " " + salary + " " + age + " " + departmant + " " + vocation;
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
 }
--- src/main/java/com/example/TestFilter/TestFilterApplication.java
+++ src/main/java/com/example/TestFilter/TestFilterApplication.java
@@ -35,32 +35,24 @@ public class TestFilterApplication
                 .setParameter("name", "Constructions");
         session.enableFilter("salaryFilter")
                 .setParameter("salary", 3000);
-//        session.enableFilter("ageFilter")
-//                .setParameter("age", 40);
+        session.enableFilter("ageFilter")
+                .setParameter("age", 40);
 
-
-//		select e.name, e.salary, e.age, d.name, v.name
-//		from department d
-//		join employee e on e.department_id=d.id
-//		join vocation v on e.vocation_id=v.id
-//		where e.salary> 3000 and e.age<40 and d.name='Constructions' and v.name='manager';
+//        Employee emp = session.get(Employee.class, 1);
 
 //		Mike	5000	25	Constructions	manager
 
-//		String select = "select new com.example.TestFilter.EmployeeDto(e) from Department d join d.employees e";
-//		String select = "select new com.example.TestFilter.EmployeeDto(e) from Department d join d.employees e join e.vocation v where v.name='manager'";
         String select;
 //        select = "select new com.example.TestFilter.EmployeeDto(e) from Department d join d.employees e join e.vocation v";
 //        select = "select new com.example.TestFilter.EmployeeDto(e) from Vocation v join v.employees e join e.department d";
 //        select = "select new com.example.TestFilter.EmployeeDto(e) from Employee e join e.department d join e.vocation v";
-        select = "select new com.example.TestFilter.EmployeeDto(e,d,v) "+
-                "from Employee e, Department d, Vocation v "+
+        select = "select new com.example.TestFilter.EmployeeDto(e,d,v) " +
+                "from Employee e, Department d, Vocation v " +
                 "where e.department=d and e.vocation=v";
 
-        List list = session.createQuery(select)
-                .list();
+        List<EmployeeDto> list = session.createQuery(select).list();
 
-        list.forEach(System.out::println);
+        list.forEach(EmployeeDto::print);
 
         session.close();
     }
